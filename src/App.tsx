b5b6699882aadb5c98d07a8e100714cbc0bccdb4
Jeroen Gerits <jeroen@gerits.email>
2022-07-04 10:53:11 +0200

    adding ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+
 const App = () => (
   <div className="bg-indigo-50 flex items-center justify-center h-screen">
     <h1 className="text-6xl font-bold text-indigo-700">Hello World!</h1>
